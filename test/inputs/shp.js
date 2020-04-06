9926bc7bbf2c7a58394e1827c0958c8e53fbe2ad
contra <yo@contra.io>
2020-04-06 16:08:03 -0400

    chore: more tests

--- test/inputs/shp.js
+++ test/inputs/shp.js
@@ -7,6 +7,9 @@ import { from } from '../../src'
 const shapeFile = join(__dirname, '../fixtures/shp-stations.zip')
 const expectedGeoFile = fs.readFileSync(join(__dirname, '../fixtures/shp-stations.geojson'), 'utf8')
 
+const shapeFile2 = join(__dirname, '../fixtures/shp-hsa-boundaries.zip')
+const expectedGeoFile2 = fs.readFileSync(join(__dirname, '../fixtures/shp-hsa-boundaries.geojson'), 'utf8')
+
 describe('from(shp)', () => {
   it('should not blow up on create', () => {
     should.exist(from('shp'))
@@ -16,5 +19,11 @@ describe('from(shp)', () => {
     const stream = inp.pipe(from('shp'))
     const res = await collect.array(stream)
     should(res).eql(JSON.parse(expectedGeoFile).features)
+  })
+  it('should translate a shapefile to geojson 2', async () => {
+    const inp = fs.createReadStream(shapeFile2)
+    const stream = inp.pipe(from('shp'))
+    const res = await collect.array(stream)
+    should(res).eql(JSON.parse(expectedGeoFile2).features)
   })
 })
