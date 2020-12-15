6d1c80e31cc9e05b6329fe85deade62f7e881402
contra <yo@contra.io>
2020-12-14 18:27:58 -0700

    chore: new tests

--- test/inputs/shp.js
+++ test/inputs/shp.js
@@ -10,6 +10,9 @@ const expectedGeoFile = fs.readFileSync(join(__dirname, '../fixtures/shp-station
 const shapeFile2 = join(__dirname, '../fixtures/shp-hsa-boundaries.zip')
 const expectedGeoFile2 = fs.readFileSync(join(__dirname, '../fixtures/shp-hsa-boundaries.geojson'), 'utf8')
 
+const shapeFile3 = join(__dirname, '../fixtures/raid-shapefile.shp.zip')
+const shapeFile4 = join(__dirname, '../fixtures/raid-shapefile-2.shp.zip')
+
 describe('from(shp)', () => {
   it('should not blow up on create', () => {
     should.exist(from('shp'))
@@ -25,5 +28,17 @@ describe('from(shp)', () => {
     const stream = inp.pipe(from('shp'))
     const res = await collect.array(stream)
     should(res).eql(JSON.parse(expectedGeoFile2).features)
+  })
+  it('should translate a shapefile to geojson 3', async () => {
+    const inp = fs.createReadStream(shapeFile3)
+    const stream = inp.pipe(from('shp'))
+    const res = await collect.array(stream)
+    should(res.length).eql(58745)
+  })
+  it('should translate a shapefile to geojson 4', async () => {
+    const inp = fs.createReadStream(shapeFile4)
+    const stream = inp.pipe(from('shp'))
+    const res = await collect.array(stream)
+    should(res.length).eql(57718)
   })
 })
